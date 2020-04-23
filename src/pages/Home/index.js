b43cb71f27f0b393d107ba525a8568234e967e74
Pedro Sena <pmfsl@cin.ufpe.br>
2020-04-23 19:12:51 -0300

    editing style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -304,7 +304,7 @@ class Home extends Component {
           </div>
         </div>
         <Footer />
-      </Container>
+      </Container >
     );
   }
 }
--- src/pages/WhoWeAre/index.js
+++ src/pages/WhoWeAre/index.js
@@ -9,13 +9,17 @@ export default function WhoWeAre() {
         <div className="external-container">
             <Header />
             <div className="info-container row">
+                <h1> Sobre </h1>
                 <div className="about-container col-md-10">
-                    <h1> Sobre </h1>
                     <span>
-                        A plataforma Observatório Covid-19 tem como objetivo monitorar a propagação do
-                        vírus nos municípios de Pernambuco para que assim possamos divulgar dados oficias
-                        à população, como os casos suspeitos, confirmados, recuperados e óbtios. Além disso
-                        a plataforma permite que o cidadão relate casos suspeitos próximos a ele.
+                        A plataforma OCovid-19 é um aplicativo do projeto Observatório Covid-19 que tem
+                        como objetivo monitorar a propagação do vírus nos municípios de Pernambuco para
+                        que assim possamos divulgar dados oficias à população, como os casos suspeitos,
+                        confirmados, recuperados e óbtios. Além disso a plataforma conta com duas redes
+                        de observadores, a primeira é uma rede aberta de observadores que permite que o
+                        cidadão relate casos suspeitos próximos a ele. E a segunda é uma rede fechada de
+                        observadores, onde observadores cadastrados são responsáveis pelos relatos dos
+                        casos na região que reside.
                     </span>
                 </div>
             </div>
