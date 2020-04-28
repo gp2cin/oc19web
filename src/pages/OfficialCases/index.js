bdf73a1e9810d70832c7a63bf6c4141d6516feb2
Pedro Sena <pmfsl@cin.ufpe.br>
2020-04-28 17:20:27 -0300

    add state data area

--- src/pages/OfficialCases/index.js
+++ src/pages/OfficialCases/index.js
@@ -7,8 +7,10 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 import api from '../../services/api';
+import moment from 'moment';
 
 export default function OfficialCases() {
+    const [state, setState] = useState({})
     const [cities, setCities] = useState([]);
     const [confirmedCases, setConfirmedCases] = useState('-');
     const [suspectCases, setSuspectCases] = useState('-');
@@ -17,19 +19,37 @@ export default function OfficialCases() {
 
     const animatedComponents = makeAnimated();
 
+    //Pegar os dados de casos oficiais de Pernambuco
     useEffect(() => {
-        //base de dados do IBGE, código de Pernambuco: 26
-        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/26/municipios`)
-            .then(res => res.json())
-            .then((data) => {
-                console.log(cities)
-                let arr = []
-                for (const i in data) {
-                    const itemToAdd = { value: `${data[i].id}`, label: `${data[i].nome}` };
-                    arr = [...arr, itemToAdd]
-                }
-                setCities(arr);
-            })
+        try {
+            api.get('api/v1/cases/state')
+                .then(response => {
+                    console.log(response)
+                    setState(response.data)
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
+
+    //Pegar os municípios do estado de Pernambuco
+    useEffect(() => {
+        try {
+            //base de dados do IBGE, código de Pernambuco: 26
+            fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/26/municipios`)
+                .then(res => res.json())
+                .then((data) => {
+                    console.log(cities)
+                    let arr = []
+                    for (const i in data) {
+                        const itemToAdd = { value: `${data[i].id}`, label: `${data[i].nome}` };
+                        arr = [...arr, itemToAdd]
+                    }
+                    setCities(arr);
+                })
+        } catch (error) {
+            console.log(error)
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
@@ -72,8 +92,34 @@ export default function OfficialCases() {
         <div className="external-container">
             <Header />
             <div className="official-cases-container row">
+                <h1>Acompanhe os casos oficiais de <strong>Pernambuco</strong> .</h1>
+                <div className="state-cases-container col-md-10">
+                    <div className="result-cases col-7">
+                        <p>Número de casos confirmados:</p>
+                        <p1>{state.confirmed ? state.confirmed : '-'}</p1>
+                    </div>
+                    <div className="result-cases col-md-7">
+                        <p>Número de óbitos:</p>
+                        <p1>{state.deaths ? state.deaths : '-'}</p1>
+                    </div>
+                    <div className="result-cases col-md-7">
+                        <p>Número de casos suspeitos:</p>
+                        <p1>{state.suspects ? state.suspects : '-'}</p1>
+                    </div>
+                    <div className="result-cases col-md-7">
+                        <p>Número de casos recuperados:</p>
+                        <p1>{state.recovered ? state.recovered : '-'}</p1>
+                    </div>
+                    <div className="result-cases col-md-7">
+                        <p>Número de casos ativos:</p>
+                        <p1>{state.active ? state.active : '-'}</p1>
+                    </div>
+                    <div className="result-cases col-md-7">
+                        <p>Letalidade:</p>
+                        <p1>{state.lethality_percentage ? state.lethality_percentage : '-'}</p1>
+                    </div>
+                </div>
                 <h1>Busque os casos registrados oficialmente nos municipios de <strong>Pernambuco</strong> .</h1>
-                <h6> Fonte: IRRD-PE.</h6>
                 <div className="search-container col-md-10">
                     <div className="city-select  col-md-6">
                         <p>Escolha uma cidade:</p>
@@ -108,6 +154,7 @@ export default function OfficialCases() {
                         <p1>{`${recoveredCases.toLocaleString()}`}</p1>
                     </div>
                 </div>
+                <h6> {state.updatedAt ? `Fonte: IRRD-PE. Atualizado em: ${moment(state.updatedAt).format('DD/MM/YYYY')}` : 'Fonte: IRRD-PE.'}</h6>
             </div>
             <Footer />
         </div>
