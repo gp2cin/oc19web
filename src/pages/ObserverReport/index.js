05681ffcbace5f78bd1cc7057d77f4217ce47043
raquel <sbsx77xavier@hotmail.com>
2020-05-08 18:35:25 -0300

    Fixing error when changing between free field or select on neighborhood

--- src/pages/ObserverReport/index.js
+++ src/pages/ObserverReport/index.js
@@ -143,6 +143,7 @@ export default function ObserverReport() {
                     });
             } else {
                 setNeighborhoods([]);
+                setIsRecifeSelected(false);
             }
         }
     }
