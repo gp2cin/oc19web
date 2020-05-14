fa0435281da8c2b2af0b6e02d44a6337f0d7ca3d
Pedro Sena <pmfsl@cin.ufpe.br>
2020-05-14 16:33:33 -0300

    remove suspects form useState(), change routes

--- src/pages/OfficialCases/index.js
+++ src/pages/OfficialCases/index.js
@@ -13,7 +13,7 @@ export default function OfficialCases() {
     const [state, setState] = useState({})
     const [cities, setCities] = useState([]);
     const [confirmedCases, setConfirmedCases] = useState('-');
-    const [suspectCases, setSuspectCases] = useState('-');
+    const [activeCases, setActiveCases] = useState('-');
     const [deaths, setDeaths] = useState('-');
     const [recoveredCases, setrecoveredCases] = useState('-');
 
@@ -22,7 +22,7 @@ export default function OfficialCases() {
     //Pegar os dados de casos oficiais de Pernambuco
     useEffect(() => {
         try {
-            api.get('api/v1/cases/official/state')
+            api.get('api/v1/cases/state')
                 .then(response => {
                     console.log(response)
                     setState(response.data)
@@ -61,7 +61,7 @@ export default function OfficialCases() {
             console.log(choice.label.toLowerCase())
             const parsedCity = choice.label.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
             console.log(parsedCity);
-            api.get(`api/v1/cases/official/state?cidade=${parsedCity}`)
+            api.get(`api/v1/cases/state?cidade=${parsedCity}`)
                 .then(response => {
                     console.log(response)
                     if (response !== null) {
@@ -70,11 +70,6 @@ export default function OfficialCases() {
                             setConfirmedCases(response.data.confirmed);
                         } else {
                             setConfirmedCases(0);
-                        }
-                        if (response.data.suspects) {
-                            setSuspectCases(response.data.suspects);
-                        } else {
-                            setSuspectCases(0);
                         }
                         if (response.data.deaths) {
                             setDeaths(response.data.deaths);
@@ -85,6 +80,11 @@ export default function OfficialCases() {
                             setrecoveredCases(response.data.recovered);
                         } else {
                             setrecoveredCases(0);
+                        }
+                        if (response.data.active) {
+                            setActiveCases(response.data.active);
+                        } else {
+                            setActiveCases(0);
                         }
                     }
                 })
@@ -106,10 +106,6 @@ export default function OfficialCases() {
                         <p1>{state.deaths ? state.deaths.toLocaleString() : '-'}</p1>
                     </div>
                     <div className="result-cases col-md-7">
-                        <p>Número de casos suspeitos:</p>
-                        <p1>{state.suspects ? state.suspects.toLocaleString() : '-'}</p1>
-                    </div>
-                    <div className="result-cases col-md-7">
                         <p>Número de casos recuperados:</p>
                         <p1>{state.recovered ? state.recovered.toLocaleString() : '-'}</p1>
                     </div>
@@ -120,6 +116,10 @@ export default function OfficialCases() {
                     <div className="result-cases col-md-7">
                         <p>Letalidade:</p>
                         <p1>{state.lethality_percentage ? ((state.lethality_percentage * 100).toPrecision(3)) + '%' : '-'}</p1>
+                    </div>
+                    <div className="result-cases col-md-7">
+                        <p>Mortalidade a cada 100 mil hab.:</p>
+                        <p1>{state.mortality_100k ? state.mortality_100k.toLocaleString() : '-'}</p1>
                     </div>
                 </div>
                 <h1>Busque os casos registrados oficialmente nos municipios de <strong>Pernambuco</strong> .</h1>
@@ -149,12 +149,12 @@ export default function OfficialCases() {
                         <p1>{`${deaths.toLocaleString()}`}</p1>
                     </div>
                     <div className="result-cases col-md-7">
-                        <p>Número de casos suspeitos:</p>
-                        <p1>{`${suspectCases.toLocaleString()}`}</p1>
-                    </div>
-                    <div className="result-cases col-md-7">
                         <p>Número de casos recuperados:</p>
                         <p1>{`${recoveredCases.toLocaleString()}`}</p1>
+                    </div>
+                    <div className="result-cases col-md-7">
+                        <p>Número de casos ativos:</p>
+                        <p1>{`${activeCases.toLocaleString()}`}</p1>
                     </div>
                 </div>
                 <h6> {state.updatedAt ? `Fonte: IRRD-PE. Atualizado em: ${moment(state.updatedAt).format('DD/MM/YYYY')}` : 'Fonte: IRRD-PE.'}</h6>
