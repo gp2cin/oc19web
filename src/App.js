18645b6a0e7d90918c6671efb16743076660c9e5
Victor sena <vsla@cin.ufpe.br>
2020-05-13 21:29:45 -0300

    testando diferentes .envs nas pipelines

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Routes from './routes/index';
 import { GlobalStyle } from './styles/global';
 
+console.log(process.env);
+
 const App = () => {
   return (
     <div className={'container'}>
