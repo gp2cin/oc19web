d6233fe8c2da87acb74f741dc5fc7be17e892b43
Victor sena <vsla@cin.ufpe.br>
2020-05-13 21:51:43 -0300

    .env diferente para cada estágio de build

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Routes from './routes/index';
 import { GlobalStyle } from './styles/global';
 
-console.log(process.env);
-
 const App = () => {
   return (
     <div className={'container'}>
