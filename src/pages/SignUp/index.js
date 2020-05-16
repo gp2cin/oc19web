1034a6e820bbfd7cb1bf01c0f1774bce6a34182f
Pedro Sena <pmfsl@cin.ufpe.br>
2020-05-16 20:33:46 -0300

    edit signup style

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import api from '../../services/api';
 import { login } from '../../services/auth';
+import Header from '../../components/Header';
 
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import DatePicker from 'react-datepicker';
 
 import Logo from '../../assets/ocovid19-logo.png';
-import { Container, Form } from './styles';
+import useStyles from './styles';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link to="/" color="inherit">
+                OCovid-19
+        </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
 export default function SignUp() {
+    const classes = useStyles();
     const [error, setError] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -21,7 +38,7 @@ export default function SignUp() {
     const [last_name, set_last_name] = useState('');
     const [gender, setGender] = useState('');
     const [birthdate, setBirthdate] = useState('');
-    const [controlDate, setControlDate] = useState(new Date());
+    const [controlDate, setControlDate] = useState();
 
     const history = useHistory();
 
@@ -66,60 +83,92 @@ export default function SignUp() {
     }
 
     return (
-        <Container>
-            <Form onSubmit={handleSignUp}>
-                <img src={Logo} alt="OC19 logo" />
-                {error && <p className="error">{error}</p>}
-                <input
-                    type={'email'}
-                    placeholder={'Endereço de e-mail'}
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type={'password'}
-                    placeholder={'Senha'}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <input
-                    placeholder={'Primeiro nome'}
-                    value={first_name}
-                    onChange={(e) => set_first_name(e.target.value)}
-                />
-                <input
-                    placeholder={'Último nome'}
-                    value={last_name}
-                    onChange={(e) => set_last_name(e.target.value)}
-                />
-                <div className="report-type">
-                    <FormControl component={'fieldset'}>
-                        <p>{'Gênero'}</p>
-                        <RadioGroup
-                            aria-label={'q'}
-                            name={'q1'}
-                            value={gender}
-                            onChange={(e) => setGender(e.target.value)}
-                        >
-                            <FormControlLabel value={'male'} control={<Radio />} label={'Masculino'} />
-                            <FormControlLabel value={'female'} control={<Radio />} label={'Feminino'} />
-                            <FormControlLabel value={'other'} control={<Radio />} label={'Outro'} />
-                        </RadioGroup>
-                    </FormControl>
-                </div>
-                <div className="birthdate">
-                    <p>{'Data de nascimento'}</p>
-                    <DatePicker
-                        maxDate={new Date()}
-                        className={'date-picker form-control'}
-                        dateFormat={'dd/MM/yyyy'}
-                        locale={'BR'}
-                        selected={controlDate}
-                        onChange={(controlDate) => handleBirthdate(controlDate)}
-                    />
-                </div>
-                <button className="button" type={'submit'}>Cadastrar</button>
-            </Form>
+        <Container component="main" maxWidth="xs">
+            <Header />
+            <div className={classes.paper}>
+                <img src={Logo} alt="OC19 logo" width="150px" />
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                {error && <p id="error">{error}</p>}
+                <form onSubmit={handleSignUp}>
+                    <div className={'form-group'}>
+                        <label className={'form-label'}>{'Nome*'}</label>
+                        <input
+                            placeholder={'Nome'}
+                            className={'form-control'}
+                            value={first_name}
+                            onChange={(e) => set_first_name(e.target.value)}
+                        ></input>
+                    </div>
+                    <div className={'form-group'}>
+                        <label className={'form-label'}>{'Sobrenome*'}</label>
+                        <input
+                            placeholder={'Sobrenome'}
+                            className={'form-control'}
+                            value={last_name}
+                            onChange={(e) => set_last_name(e.target.value)}
+                        ></input>
+                    </div>
+                    <div className={'form-group'}>
+                        <label className={'form-label'}>{'E-mail*'}</label>
+                        <input
+                            placeholder={'example@email.com'}
+                            className={'form-control'}
+                            value={email}
+                            type={'e-mail'}
+                            onChange={(e) => setEmail(e.target.value)}
+                        ></input>
+                    </div>
+                    <div className={'form-group'}>
+                        <label className={'form-label'}>{'Senha*'}</label>
+                        <input
+                            placeholder={'Senha'}
+                            className={'form-control'}
+                            type={'password'}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        ></input>
+                    </div>
+                    <div className={'form-group'}>
+                        <FormControl component={'fieldset'}>
+                            <p>{'Gênero'}</p>
+                            <RadioGroup
+                                aria-label={'q'}
+                                name={'q1'}
+                                value={gender}
+                                onChange={(e) => setGender(e.target.value)}
+                            >
+                                <FormControlLabel value={'male'} control={<Radio />} label={'Masculino'} />
+                                <FormControlLabel value={'female'} control={<Radio />} label={'Feminino'} />
+                                <FormControlLabel value={'other'} control={<Radio />} label={'Outro'} />
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
+                    <div className={'form-group'}>
+                        <label className={'form-label'}>{'Data de Nascimento*'}</label>
+                        <DatePicker
+                            maxDate={new Date()}
+                            className={'date-picker form-control ml-1'}
+                            dateFormat={'dd/MM/yyyy'}
+                            locale={'BR'}
+                            selected={controlDate}
+                            onChange={(controlDate) => handleBirthdate(controlDate)}
+                        />
+                    </div>
+                    <button className={'btn btn-primary col-md-12'} type={'submit'}>
+                        {'Cadastrar'}
+                    </button>
+                    <Grid container>
+                        <Grid item>
+                            <Link to="/signin" variant="body2">
+                                Já tem uma conta? Faça o login.
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Copyright />
         </Container>
     );
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,51 +1,12 @@
-import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-`;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  }
+}));
 
-export const Form = styled.form`
-  width: 400px;
-  background: #fff;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  img {
-    width: 100px;
-    margin: 10px 0 40px;
-  }
-  .error {
-    color: #ff3333;
-    margin-bottom: 15px;
-    border: 1px solid #ff3333;
-    padding: 10px;
-    width: 100%;
-    text-align: center;
-  }
-  input {
-    flex: 1;
-    height: 46px;
-    margin-bottom: 15px;
-    padding: 0 20px;
-    color: #777;
-    font-size: 15px;
-    width: 100%;
-    border: 1px solid #ddd;
-    &::placeholder {
-      color: #999;
-    }
-  }
-  .button {
-    color: #fff;
-    font-size: 16px;
-    background: #fc6963;
-    height: 56px;
-    border: 0;
-    border-radius: 5px;
-    width: 100%;
-  }
-`;
+export default useStyles
