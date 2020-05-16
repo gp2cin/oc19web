2df6701438a0b800c1e8d8d57050de66be0de491
Pedro Sena <pmfsl@cin.ufpe.br>
2020-05-16 20:38:31 -0300

    edit api file

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { getToken } from './auth';
+const { REACT_APP_API_URL } = process.env;
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: REACT_APP_API_URL,
 });
 
 
