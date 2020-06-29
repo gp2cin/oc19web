ca3ad7c8d33b715bc6e3ec628068a31c38d7aa28
Pedro Sena <pmfsl@cin.ufpe.br>
2020-06-29 19:11:13 -0300

    change observation field for textarea

--- src/pages/GeneralWebsite/GeneralObservationNotLogged/index.js
+++ src/pages/GeneralWebsite/GeneralObservationNotLogged/index.js
@@ -343,8 +343,9 @@ export default function GeneralObservation() {
                 <div className="comments col-md-12">
                     <div className="col-md-12" style={{ padding: '0px' }}>
                         <p>Observação:*</p>
-                        <input
-                            placeholder="Observação"
+                        <textarea
+                            rows={10}
+                            placeholder="Digite sua observação aqui"
                             className="col-md-12 form-control"
                             value={observation}
                             style={requiredInputStyle.observation}
@@ -352,7 +353,7 @@ export default function GeneralObservation() {
                                 setObservation(e.target.value)
                                 setRequiredInputStyle(prev => ({ ...prev, observation: {} }))
                             }}
-                        ></input>
+                        ></textarea>
                     </div>
                 </div>
 
