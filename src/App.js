b7acb5b8ed3e524b1e254c89fa6708cd6f2dc07b
Victor sena <vsla@cin.ufpe.br>
2020-08-03 18:53:57 -0300

    dashboard atualizada

--- src/App.js
+++ src/App.js
@@ -7,24 +7,29 @@ import theme from './theme';
 import { Chart } from 'react-chartjs-2';
 import { ThemeProvider } from '@material-ui/styles';
 
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import './assets/scss/index.scss';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
+moment.locale('br');
 
 Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
   draw: chartjs.draw,
 });
 
-
-
 const App = () => {
-  console.log(process.env);
-  
   return (
     <ThemeProvider theme={theme}>
-      {/* <div className={'container'}> */}
-      <Routes />
-      <GlobalStyle />
-      {/* </div> */}
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        {/* <div className={'container'}> */}
+        <Routes />
+        <GlobalStyle />
+        {/* </div> */}
+      </MuiPickersUtilsProvider>
     </ThemeProvider>
   );
 };
--- src/pages/Analyst/Reports/index.js
+++ src/pages/Analyst/Reports/index.js
@@ -2,11 +2,17 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { Select, Paper, Grid, Typography, Button, CircularProgress } from '@material-ui/core';
+import { Select, Paper, Grid, Typography, Button, CircularProgress, TextField } from '@material-ui/core';
 
 import MenuItem from '@material-ui/core/MenuItem';
-import ReportTable from './ReportTable';
+// import ReportTable from './ReportTable';
 import api from '../../../services/api';
+
+import Table2 from './Table2';
+
+import { cities } from './data';
+import { DatePicker } from '@material-ui/pickers';
+import moment from 'moment';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -49,12 +55,37 @@ export default function Reports() {
   const [secondBar, setSecondBar] = React.useState(observerTypes);
   const [actualConfig, setActualConfig] = React.useState({ agent: agentType[0], type: secondBar[0] });
 
+  const [city, setCity] = React.useState(cities[0]);
+  const [openSelectNeighborhood, setOpenSelectNeighborhood] = React.useState(false);
+
+  const [neighborhoodList, setNeighborhoodList] = React.useState([]);
+  const [loadingList, setLoadingList] = React.useState(false);
+
+  const [neighborhood, setNeighborhood] = React.useState('');
+  const [dataRange, setDataRange] = React.useState(moment());
+
   const [loading, setLoading] = React.useState(true);
   const [data, setData] = React.useState([]);
 
   useEffect(() => {
     getData();
   }, []);
+
+  // Ao selecionar uma cidade
+  useEffect(() => {
+    openSelectNeighborhood && getNeighborhoods();
+  }, [openSelectNeighborhood]);
+
+  const getNeighborhoods = async () => {
+    setLoadingList(true);
+    const response = await api.get('api/v1/neighborhoods?cidade=recife');
+    const list = response.data.map(({ name }) => name);
+    try {
+      setNeighborhoodList(list);
+      setNeighborhood(list[0]);
+    } catch (error) {}
+    setLoadingList(false);
+  };
 
   const handleChange = (e, newValue) => {
     setValue(e.target.value);
@@ -76,7 +107,13 @@ export default function Reports() {
 
     if (value === 0) {
       if (observerTypes[observerType] === 'Observações Gerais') {
-        const response = await api.get('api/v1/general-observations');
+        const filters = {
+          agentType: agentType[value],
+          observerType: secondBar[observerType],
+          city,
+          neighborhood,
+        };
+        const response = await api.get('api/v1/general-observations', { params: filters });
 
         setData(response.data.generalObservations);
       }
@@ -88,51 +125,157 @@ export default function Reports() {
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Paper style={{ padding: 15 }}>
-          <Grid container direction="row" alignItems="center" spacing={3}>
-            <Grid item>
-              <Grid container direction="row" spacing={2}>
-                <Grid item style={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography>Agente</Typography>
-                </Grid>
+          <Grid container direction="column" spacing={2}>
+            <Grid item xs={12}>
+              <Grid container direction="row" alignItems="center" spacing={3}>
                 <Grid item>
-                  <Select
-                    value={value}
-                    margin="dense"
-                    variant="outlined"
-                    onChange={handleChange}
-                    style={{ width: 150 }}
-                  >
-                    {agentType.map((name, index) => {
-                      return <MenuItem value={index}>{name}</MenuItem>;
-                    })}
-                  </Select>
+                  <Grid container direction="row" spacing={2}>
+                    <Grid item style={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography>Agente</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Select
+                        value={value}
+                        margin="dense"
+                        variant="outlined"
+                        onChange={handleChange}
+                        style={{ width: 150 }}
+                      >
+                        {agentType.map((name, index) => {
+                          return <MenuItem value={index}>{name}</MenuItem>;
+                        })}
+                      </Select>
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item>
+                  <Grid container direction="row" spacing={2}>
+                    <Grid item style={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography>Tipo: </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Select
+                        value={observerType}
+                        margin="dense"
+                        variant="outlined"
+                        onChange={changeObserverType}
+                        style={{ width: 220 }}
+                      >
+                        {secondBar.map((name, i) => {
+                          return <MenuItem value={i}>{name}</MenuItem>;
+                        })}
+                      </Select>
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item>
+                  <Grid container direction="row" spacing={2}>
+                    <Grid item style={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography>Cidade: </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Select
+                        value={city}
+                        margin="dense"
+                        variant="outlined"
+                        onChange={(e, data) => {
+                          setCity(data.props.value);
+                          if (data.props.value === 'Recife') {
+                            setOpenSelectNeighborhood(true);
+                          }
+                        }}
+                        style={{ width: 220 }}
+                      >
+                        {cities.map((name) => {
+                          return (
+                            <MenuItem key={name} value={name}>
+                              {name}
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item>
+                  <Grid container direction="row" spacing={2}>
+                    <Grid item style={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography>Bairro: </Typography>
+                    </Grid>
+                    <Grid item>
+                      {openSelectNeighborhood ? (
+                        <>
+                          {loadingList ? (
+                            <CircularProgress />
+                          ) : (
+                            <Select
+                              value={neighborhood}
+                              margin="dense"
+                              variant="outlined"
+                              onChange={(e, data) => setNeighborhood(data.props.value)}
+                              style={{ width: 220 }}
+                            >
+                              {neighborhoodList.map((name) => {
+                                return (
+                                  <MenuItem key={name} value={name}>
+                                    {name}
+                                  </MenuItem>
+                                );
+                              })}
+                            </Select>
+                          )}
+                        </>
+                      ) : (
+                        <TextField />
+                      )}
+                    </Grid>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item>
-              <Grid container direction="row" spacing={2}>
-                <Grid item style={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography>Tipo: </Typography>
-                </Grid>
+
+            <Grid item xs={12}>
+              <Grid container direction="row" alignItems="center" spacing={3}>
                 <Grid item>
-                  <Select
-                    value={observerType}
-                    margin="dense"
-                    variant="outlined"
-                    onChange={changeObserverType}
-                    style={{ width: 220 }}
-                  >
-                    {secondBar.map((name, i) => {
-                      return <MenuItem value={i}>{name}</MenuItem>;
-                    })}
-                  </Select>
+                  <Grid container alignItems="center" direction="row" spacing={2}>
+                    <Grid item style={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography>Data</Typography>
+                    </Grid>
+                    <Grid item>
+                      <DatePicker
+                        margin="dense"
+                        inputVariant="outlined"
+                        format="DD/MM/yyyy"
+                        value={dataRange}
+                        onChange={setDataRange}
+                        animateYearScrolling
+                      />
+                    </Grid>
+                    <Grid item style={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography> para </Typography>
+                    </Grid>
+                    <Grid item>
+                      <DatePicker
+                        margin="dense"
+                        inputVariant="outlined"
+                        format="DD/MM/yyyy"
+                        value={dataRange}
+                        onChange={setDataRange}
+                        animateYearScrolling
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item>
+                  <Button variant="outlined" onClick={() => handleSearch()}>
+                    Buscar
+                  </Button>
                 </Grid>
               </Grid>
-            </Grid>
-            <Grid item>
-              <Button variant="outlined" onClick={() => handleSearch()}>
-                Buscar
-              </Button>
             </Grid>
           </Grid>
         </Paper>
@@ -145,7 +288,8 @@ export default function Reports() {
             </Grid>
           </Grid>
         ) : (
-          <ReportTable agent={actualConfig.agent} type={actualConfig.type} data={data} />
+          // <ReportTable agent={actualConfig.agent} type={actualConfig.type} data={data} />
+          <Table2 agent={actualConfig.agent} type={actualConfig.type} data={data} />
         )}
       </Grid>
     </Grid>
